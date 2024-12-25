cc71256998ff8a562150db5512fc22770eb91eea
Mourish Antony <mourishantonyc@gmail.com>
2024-12-25 22:08:26 +0530

    Myfirstcommit

---
+++ ant.py
@@ -0,0 +1,10 @@
+def greatest_of_two_numbers(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+# Example usage
+num1 = 10
+num2 = 20
+print("The greatest number is:", greatest_of_two_numbers(num1, num2))
